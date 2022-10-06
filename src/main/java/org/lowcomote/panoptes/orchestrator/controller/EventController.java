55d438d62c9e1ef34136605156ae6418841de33f
Panagiotis Kourouklidis <panagiotis.kourouklidis@bt.com>
2022-10-06 12:27:51 +0100

    Added logging for received Cloud Events

--- src/main/java/org/lowcomote/panoptes/orchestrator/controller/EventController.java
+++ src/main/java/org/lowcomote/panoptes/orchestrator/controller/EventController.java
@@ -1,6 +1,8 @@
 package org.lowcomote.panoptes.orchestrator.controller;
 
 import org.lowcomote.panoptes.orchestrator.service.EventService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,9 +14,11 @@ import io.cloudevents.CloudEvent;
 public class EventController {
 	@Autowired
 	private EventService eventService;
+	Logger logger = LoggerFactory.getLogger(EventController.class);
 	
 	@PostMapping(value = "/api/v1/events")
 	void ingestEvent(@RequestBody CloudEvent event) throws Exception {
+		logger.info("Received Cloud Event of type: " + event.getType() + " with data: " + event.getData().toString());
 		eventService.ingestEvent(event);
 	}
 
